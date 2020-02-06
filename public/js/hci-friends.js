07cc7ccedd655b483148ec883eea8f807d9048d5
Amy <aan@ucsd.edu>
2020-02-06 10:28:32 -0800

    first commit

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,8 +3,14 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
 
+$(".friendName").click(function(e) {
+		e.preventDefault();
+		var anagram = anagrammedName($(this).text());
+		$(this).text(anagram);
+		console.log("clicked");
+	});
+  })
 /*
  * Function that is called when the document is ready.
  */
@@ -14,10 +20,10 @@ function initializePage() {
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
